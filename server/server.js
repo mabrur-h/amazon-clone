c5b7ecdb0600f2a49c1b70dd369442163ec22503
mabrur-h <mabrur.h11@gmail.com>
2021-04-20 23:13:57 +0500

    add user and product schemas

--- server/server.js
+++ server/server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+const User = require('./models/UserModel')
+
+dotenv.config();
 
 const app = express();
+
+mongoose.connect(process.env.DATABASE,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err) => {
+    if ( err ) console.log (err)
+    else console.log ('connected to the database')
+})
 
 // middlewares
 app.use(morgan('dev'));
@@ -19,11 +31,22 @@ app.get('/', (req, res) => {
 
 // send data from frontend to backend
 app.post('/', (req, res) => {
-    console.log (req.body)
+    let user = new User();
+    user.name = req.body.name;
+    user.email = req.body.email;
+    user.password = req.body.password;
+
+    user.save((err) => {
+        if (err) res.json (err)
+        else res.json('successfully saved')
+    })
 })
 
 
-app.listen(3000, (err) => {
+app.listen(process.env.PORT, (err) => {
     if ( err ) console.log (err);
-    else console.log ('working')
+    else console.log (`
+You're working at port ${process.env.PORT}
+http://localhost:3000/
+`)
 })
