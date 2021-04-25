0975ac0966b1f3a7713ecde6dae7bc12c595510f
mabrur-h <mabrur.h11@gmail.com>
2021-04-25 18:09:08 +0500

    add admin and client side

---
+++ client/components/Footer.vue
@@ -0,0 +1,164 @@
+<template>
+  <!--FOOTER-->
+  <footer>
+    <div class="navLeftFooter p-0">
+      <a href="#" id="navBackToTop">
+        <div class="navFooterBackToTop">
+          <span class="navFooterBackToTopText">Back to top</span>
+        </div>
+      </a>
+      <div class="container-fluid">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-3 col-sm-6">
+              <div class="navFooterCol">
+                <div class="navFooterColHead">Get to Know Us</div>
+                <ul class="ml-0 pl-0">
+                  <li>
+                    <a href="#">Careers</a>
+                  </li>
+                  <li>
+                    <a href="#">Blog</a>
+                  </li>
+                  <li>
+                    <a href="#">About Amazon</a>
+                  </li>
+                  <li>
+                    <a href="#">Investor Relations</a>
+                  </li>
+                  <li>
+                    <a href="#">Amazon Devices</a>
+                  </li>
+                  <li>
+                    <a href="#">Amazon Tours</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+              <div class="navFooterCol">
+                <div class="navFooterColHead">Make Money with Us</div>
+                <ul class="ml-0 pl-0">
+                  <li>
+                    <a href="#">Sell on Amazon</a>
+                  </li>
+                  <li>
+                    <a href="#">Blog</a>
+                  </li>
+                  <li>
+                    <a href="#">About Amazon</a>
+                  </li>
+                  <li>
+                    <a href="#">Investor Relations</a>
+                  </li>
+                  <li>
+                    <a href="#">Amazon Devices</a>
+                  </li>
+                  <li>
+                    <a href="#">Amazon Tours</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+              <div class="navFooterCol">
+                <div class="navFooterColHead">Amazon Payment Products</div>
+                <ul class="ml-0 pl-0">
+                  <li>
+                    <a href="#">Sell on Amazon</a>
+                  </li>
+                  <li>
+                    <a href="#">Blog</a>
+                  </li>
+                  <li>
+                    <a href="#">About Amazon</a>
+                  </li>
+                  <li>
+                    <a href="#">Investor Relations</a>
+                  </li>
+                  <li>
+                    <a href="#">Amazon Devices</a>
+                  </li>
+                  <li>
+                    <a href="#">Amazon Tours</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+              <div class="navFooterCol">
+                <div class="navFooterColHead">Let Us Help You</div>
+                <ul class="ml-0 pl-0">
+                  <li>
+                    <a href="#">Sell on Amazon</a>
+                  </li>
+                  <li>
+                    <a href="#">Blog</a>
+                  </li>
+                  <li>
+                    <a href="#">About Amazon</a>
+                  </li>
+                  <li>
+                    <a href="#">Investor Relations</a>
+                  </li>
+                  <li>
+                    <a href="#">Amazon Devices</a>
+                  </li>
+                  <li>
+                    <a href="#">Amazon Tours</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="nav-footer-line"></div>
+        <div class="container">
+          <div class="navFooterPadItem row">
+            <div class="col-md-4 col-sm-12 text-right">
+              <div class="navFooterLogoLine">
+                <a href="#">
+                  <div class="nav-logo-base nav-sprite"></div>
+                </a>
+              </div>
+            </div>
+            <div class="col-md-8 col-sm-12">
+              <span class="icp-container-desktop">
+                <div class="navFooterLine">
+                  <a href="#" class="icp-button">
+                    <div class="icp-nav-globe-img-2 icp-button-globe-2 icp-nav-globe-curr-fix">​</div>
+                    <span class="icp-color-base">English</span>
+                    <span class="nav-arrow icp-up-down-arrow"></span>
+                  </a>
+                  <a href="#" class="icp-button">
+                    <span class="icp-currency-symbol">$</span>
+                    <span class="icp-color-base">USD - U.S. Dollar</span>
+                  </a>
+                  <a href="#" class="icp-button">
+                    <span class="icp-currency-symbol">$</span>
+                    <span class="icp-color-base">United States</span>
+                  </a>
+                </div>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="navFooterCopyright mt-2">
+        <ul class="mt-2">
+          <li>
+            <a href="#" class="nav_a">Conditions of Use</a>
+          </li>
+          <li>
+            <a href class="nav_a">Privacy Notice</a>
+          </li>
+          <li>
+            <a href class="nav_a">Internet-Based Ads</a>
+          </li>
+          <li class="nav_last">© 1996-2021, Amazon.com, Inc. or its affiliates</li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+  <!--/FOOTER-->
+</template>
---
+++ client/layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Navbar />
+    <Nuxt />
+    <Footer />
+  </div>
+</template>
+<script>
+import Navbar from "../components/Navbar";
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    Navbar
+  }
+
+}
+</script>
+<style>
+
+</style>
